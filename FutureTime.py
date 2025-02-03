9364276705da0b206107fb630a762d6e89243504
AlexHernandez2006 <ahernandezlopez@unomaha.edu>
2025-02-03 02:07:19 +0000

    Done with lab.

--- FutureTime.py
+++ FutureTime.py
@@ -1,7 +1,7 @@
 #FutureTime.py
-#Name:
-#Date:
-#Assignment:
+#Name: Alex Hernandez Lopez
+#Date: 2/2/25
+#Assignment: Lab 2
 
 # datetime will allow us to access the system date and time.
 import datetime
@@ -9,17 +9,22 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 6) % 24
   currentMinute = now.minute
 
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
+  #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
+  hours = input("Enter number of hours: ")
   #Ask user for minutes
-
+  moreMins = input("Enter number of minutes: ")
   #Calculate the time after the user-supplied time has passed.
-
+  futureMins = (currentMinute + int(moreMins)) % 60
+  extraHour = (currentMinute + int(moreMins)) // 60
+  futureTime = (currentHour + int(hours) + extraHour) % 24
+  print(futureTime)
+  print(futureMins)
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,18 +1,24 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Alex Hernandez Lopez
+#Date: 2/2/25
+#Assignment: Lab 2
 
 #We will need random for this program, import to use this package.
 import random
 
 def main():
   #Create a list of your responses.
-  print("Magic 8 Ball")
+ print("Magic 8 Ball")
   #Prompt the user for their question.
-
+answers = ["As I see it, yes", "Ask again later", "Better not tell you now", 
+             "Cannot predict now", "Concentrate and ask again", "Don't count on it",
+             "It is certain", "It is decidedly so", "Most likely", "My reply is no"]
+input("Ask a question: ")
   #Answer question randomly with one of the options from your earlier list.
-
-
+length = len(answers)
+r = random.random() *  length
+r = int(r)
+response = answers[r]
+print(response)
 if __name__ == '__main__':
   main()
